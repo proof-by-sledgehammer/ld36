fc68a7595aabbfcd7925279d2f9e3df5a01951a5
Daniel Spaniol <ds3@ou.nl>
2022-01-07 19:37:13 +0100

    Add dummy main menu

---
+++ screens/main_menu_screen.gd
@@ -0,0 +1,18 @@
+extends Control
+
+# ========= Event Handlers
+
+func _on_New_Game_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Arcade_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_Exit_pressed() -> void:
+	get_tree().quit()
