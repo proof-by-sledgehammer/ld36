7d83b2f32f16e204cfc07578e1cba2564c0c7731
Daniel Spaniol <ds3@ou.nl>
2022-01-07 21:41:30 +0100

    Add player cart

--- screens/ingame_screen.gd
+++ screens/ingame_screen.gd
@@ -4,6 +4,18 @@ func load_selected_level() -> void:
 	var Level = load("res://levels/level_%s.tscn" % State.selected_level)
 	var level = Level.instance()
 	add_child(level)
-	
+
+func pause_physics() -> void:
+	$Cart.mode = RigidBody2D.MODE_STATIC
+
+func unpause_physics() -> void:
+	$Cart.mode = RigidBody2D.MODE_CHARACTER
+
+
 func _ready() -> void:
 	load_selected_level()
+	pause_physics()
+
+
+func _on_Upgrade_Dialog_on_start():
+	unpause_physics()
